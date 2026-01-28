306e29601beddacfe391ceb5742b94e9f8e912a1
Alojz Milicevic <alojz.milicevic@geoguessr.com>
2026-01-28 20:44:28 +0100

    Updated config

--- docker-compose-up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "========================================"
-echo "Starting n8n..."
-echo "Access n8n at: http://localhost:5678"
-echo "========================================"
-echo ""
-
-docker compose up "$@"
